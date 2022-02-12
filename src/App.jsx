8a1b48fe9c8bea7d265eb65a5f29486185996fc2
Bartosz Zakrzewski <barzak200@gmail.com>
2022-02-12 16:57:56 +0100

    Header is now sticky

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import Header from "./components/Layout/Header/Header";
 
 const App = () => {
   return (
-    <div>
-      <div className="container">
-        <Header />
-      </div>
+    <>
+      <Header />
       <div className="body-container">
         <div className="container">
           <Body />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./Navigation";
 import SearchBar from "./SearchBar";
 
@@ -10,23 +10,40 @@ const Header = () => {
   const isLessThan1000px = useMediaQuery({ query: "(max-width: 1030px)" });
   const isLessThan600px = useMediaQuery({ query: "(max-width: 600px)" });
 
+  const isSticky = e => {
+    const header = document.getElementById("header");
+    const scrollTop = window.scrollY;
+    scrollTop >= 70
+      ? header.classList.add("sticky")
+      : header.classList.remove("sticky");
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  });
+
   return (
-    <header className={`${classes["header"]}`}>
-      <div className={classes["header-flex"]}>
-        <img
-          className={`${
-            !isLessThan600px
-              ? classes["header-logo"]
-              : classes["header-logo-small"]
-          }`}
-          src="https://assets.allegrostatic.com/metrum/brand/allegro-347440b030.svg"
-          alt="Allegro.pl - wygodne i bezpieczne zakupy online, największy wybór ofert"
-        />
-        {!isLessThan1000px && <SearchBar />}
-        <Navigation />
-      </div>
-      {isLessThan1000px && <SearchBar isMobile={isLessThan600px} />}
-    </header>
+    <div id="header">
+      <header className={`${classes["header"]} container`}>
+        <div className={classes["header-flex"]}>
+          <img
+            className={`${
+              !isLessThan600px
+                ? classes["header-logo"]
+                : classes["header-logo-small"]
+            }`}
+            src="https://assets.allegrostatic.com/metrum/brand/allegro-347440b030.svg"
+            alt="Allegro.pl - wygodne i bezpieczne zakupy online, największy wybór ofert"
+          />
+          {!isLessThan1000px && <SearchBar />}
+          <Navigation />
+        </div>
+        {isLessThan1000px && <SearchBar isMobile={isLessThan600px} />}
+      </header>
+    </div>
   );
 };
 
