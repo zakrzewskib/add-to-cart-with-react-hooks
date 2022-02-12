20ec4ef78c6ee98c1cf24fa7d96dfdb5aed633a6
Bartosz Zakrzewski <barzak200@gmail.com>
2022-02-12 12:40:28 +0100

    Add SearchBar styles

--- src/components/Layout/SearchBar.jsx
+++ src/components/Layout/SearchBar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import classes from "./SearchBar.module.css";
+
 const SearchBar = () => {
   return (
-    <form>
-      <input type="text" placeholder="czego szukasz?" />
-      <button>szukaj wielu</button>
-      <div>Telefony i Akcesoria -icon-</div>
-      <button>Szukaj</button>
+    <form className={classes["search-form"]}>
+      <div className={classes["input-container"]}>
+        <input type="text" placeholder="czego szukasz?" size="30" />
+        <button className={classes["link-button"]}>szukaj wielu</button>
+      </div>
+      <div className={classes["categories"]}>
+        Telefony i Akcesoria
+        <img
+          src="https://assets.allegrostatic.com/metrum/icon/arrowhead-9148b8f39c.svg"
+          alt=""
+        />
+      </div>
+      <button className={classes["primary-button"]}>Szukaj</button>
     </form>
   );
 };
