f1b22c604e301be94323c8a440c95c1a0bbfd817
Bartosz Zakrzewski <barzak200@gmail.com>
2022-02-13 13:57:30 +0100

    Fix input width in SearchBar

--- src/components/Layout/Header/SearchBar.jsx
+++ src/components/Layout/Header/SearchBar.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./SearchBar.module.css";
 
 const SearchBar = props => {
+  const [focused, setFocused] = useState(false);
+
+  const onFocus = () => setFocused(true); // TODO add animation on disappearing button
+  const onBlur = () => setFocused(false);
+
   return (
-    // TODO - 'szukaj wielu' should disappear when user types sth in input
     <form className={classes["search-form"]}>
       <div className={classes["input-container"]}>
-        <input type="text" placeholder="czego szukasz?" />
-        <button className={classes["link-button"]}>szukaj wielu</button>
+        <input
+          type="text"
+          placeholder="czego szukasz?"
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        {!focused && (
+          <button className={classes["link-button"]}>szukaj wielu</button>
+        )}
       </div>
       {!props.isMobile && (
         <div className={classes["categories-and-search"]}>
