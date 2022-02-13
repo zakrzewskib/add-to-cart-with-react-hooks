e38808dd098bb1fc75e3a3997b5fc62a4d772b58
Bartosz Zakrzewski <barzak200@gmail.com>
2022-02-13 11:05:35 +0100

    Style items in cart modal

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import classes from "./CartItem.module.css";
+
 const CartItem = props => {
   const item = props.item;
   return (
-    <div>
+    <div className={classes["cart-item"]}>
       <p> {item.name}</p>
       <p>{item.price}</p>
       <p>{item.amount}</p>
+      <button>x</button>
     </div>
   );
 };
